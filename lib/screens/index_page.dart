ddbac2985132813d7741f53a71e91d96e022e92f
rizveejack <jrizvee@gmail.com>
2022-06-16 17:06:00 +0600

    add nav bar

---
+++ lib/screens/index_page.dart
@@ -0,0 +1,59 @@
+// ignore_for_file: non_constant_identifier_names, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import '../widgets/custom_bottom_nav_bar.dart';
+
+class IndexPage extends StatefulWidget {
+  const IndexPage({Key? key}) : super(key: key);
+
+  @override
+  State<IndexPage> createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  final _screens = <Widget>[
+    Container(
+      child: Center(
+        child: Text("index0"),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text("index1"),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text("index2"),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text("index3"),
+      ),
+    ),
+  ];
+
+  int _currentIndex = 0;
+  void _onTab(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      bottomNavigationBar:
+          CustomBottomNavBar(currentIndex: _currentIndex, onTab: _onTab),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: _screens[_currentIndex],
+    );
+  }
+}
---
+++ lib/widgets/custom_bottom_nav_bar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  const CustomBottomNavBar({
+    Key? key,
+    required this.currentIndex,
+    required this.onTab,
+  }) : super(key: key);
+
+  final int currentIndex;
+  final Function onTab;
+
+  void changeIndex(int index) {
+    onTab(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      shape: CircularNotchedRectangle(),
+      notchMargin: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              iconSize: 28,
+              icon: Icon(
+                Icons.widgets_rounded,
+                color: currentIndex == 0 ? Colors.purple : Colors.black54,
+              ),
+              onPressed: () {
+                changeIndex(0);
+              },
+            ),
+            IconButton(
+              iconSize: 28,
+              icon: Icon(
+                Icons.local_fire_department_rounded,
+                color: currentIndex == 1 ? Colors.redAccent : Colors.black54,
+              ),
+              onPressed: () {
+                changeIndex(1);
+              },
+            ),
+            SizedBox(width: 25),
+            IconButton(
+              iconSize: 28,
+              icon: Icon(
+                Icons.shield_rounded,
+                color: currentIndex == 2 ? Colors.green : Colors.black54,
+              ),
+              onPressed: () {
+                changeIndex(2);
+              },
+            ),
+            IconButton(
+              iconSize: 28,
+              icon: Icon(
+                Icons.headset_mic_rounded,
+                color: currentIndex == 3 ? Colors.deepOrange : Colors.black54,
+              ),
+              onPressed: () {
+                changeIndex(3);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
